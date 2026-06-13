9d07b69fee87917c5356a8dad0c29afd5b8e49ad
Ganeshsurya2602 <181932122+Ganeshsurya2602@users.noreply.github.com>
2026-06-13 23:08:24 +0530

    working code

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from router import router_task
 st.title("AI Developer Assistant")
 prompt = st.text_area("Ask coding question")
 if st.button("Run AI Assistant"):
-    response = router_task(prompt)
-    st.markdown("### AI Response:")
-    st.code(response)
+    if prompt.strip():
+        response = router_task(prompt)
+
+        st.markdown("### AI Response")
+        st.code(response, language="python")
+    else:
+        st.warning("Please enter a prompt")
--- llm.py
+++ llm.py
@@ -1,11 +1,22 @@
 import os
-
+from dotenv import load_dotenv
 from groq import Groq
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-def ask_llm(message):
+load_dotenv()
+
+api_key = os.getenv("GROQ_API_KEY")
+
+if not api_key:
+    raise ValueError("GROQ_API_KEY not found")
+
+print("API KEY =", api_key)
+
+client = Groq(api_key=api_key)
+
+def ask_llm(messages):
     response = client.chat.completions.create(
-        model = "llama-3.1-8b-instant",
-        messages = message
+        model="llama-3.1-8b-instant",
+        messages=messages
     )
+
     return response.choices[0].message.content
